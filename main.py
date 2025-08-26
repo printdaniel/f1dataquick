edb686c7d84c98fb2ab98d3e369f91c407cf3521
Daniel Suárez <dwsuarez98@gmail.com>
2025-08-26 19:09:17 -0300

    menu: listo

--- main.py
+++ main.py
@@ -6,26 +6,38 @@ from setup_fastf1 import setup_fastf1_cache
 # Configurar cache
 setup_fastf1_cache()
 
-def plot_race_pace(year=2025, gp="Australia"):
-    # Cargar sesión de carrera
-    session = fastf1.get_session(year, gp, "R")
-    session.load()
 
-    # Obtener datos de vueltas
-    laps = session.laps.pick_quicklaps()
 
-    # Agrupar tiempos medios por piloto
-    avg_laps = laps.groupby("Driver")["LapTime"].mean().sort_values()
 
-    # Gráfico
-    plt.figure(figsize=(10,6))
-    sns.barplot(x=avg_laps.index, y=avg_laps.values, palette="coolwarm")
-    plt.title(f"Ritmo promedio - {gp} {year}")
-    plt.ylabel("Tiempo medio de vuelta (s)")
-    plt.xticks(rotation=45)
-    plt.tight_layout()
-    plt.show()
+
+
+
+
+
+
+
+def menu_principal():
+    while True:
+        # 1. Mostramos las opciones del menú
+        print("\n--- Menú Principal ---")
+        print("[ 1 ]. Comparar ritmo de Carrera entre pilotos")
+        print("[ 2 ]. Ritmo de carrera de un piloto específico")
+        print("[ 3 ]. Salir")
+
+        # 2. Pedimos al usuario que elija una opción
+        opcion = input("Elige una opción (1, 2, 3): ")
+
+        # 3. Manejamos la opción elegida con condicionales
+        if opcion == '1':
+            accion_1()
+        elif opcion == '2':
+            accion_2()
+        elif opcion == '3':
+            salir()
+            break # Salimos del bucle
+        else:
+            print("Opción no válida. Por favor, elige un número del 1 al 3.")
 
 if __name__ == "__main__":
-    plot_race_pace()
+    menu_principal()
 
