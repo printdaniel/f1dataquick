ce1e89def02d41765434ad724270edf2d84d4f02
Daniel Suárez <dwsuarez98@gmail.com>
2025-09-28 20:47:38 -0300

    Estructura: constantes

---
+++ constantes.py
@@ -0,0 +1,43 @@
+driver_colors = {
+    # Red Bull
+    "VER": "#1E5BC6",  # Verstappen - Azul oscuro de Red Bull
+    "TSU": "#1E5BC6",  # Tsunoda
+
+    # Ferrari
+    "LEC": "#ED1C24",  # Leclerc - Rojo Ferrari
+    "HAM": "#ED1C24",  # Hamilton
+
+    # Mercedes
+    "RUS": "#00A3B9",  # Russell - Cian/verde azulado de Mercedes
+    "ANT": "#00A3B9",  # Antonelli
+
+    # McLaren
+    "NOR": "#FF8000",  # Norris - Naranja papaya de McLaren
+    "PIA": "#FF8000",  # Piastri
+
+    # Aston Martin
+    "ALO": "#2D8266",  # Alonso - Verde británico de Aston Martin
+    "STR": "#2D8266",  # Stroll
+
+    # Alpine
+    "GAS": "#0093D0",  # Gasly - Azul claro de Alpine
+    "COL": "#0093D0",  # Colapinto
+
+    # Williams
+    "ALB": "#005AFF",  # Albon - Azul intenso de Williams
+    "SAI": "#005AFF",  # Sainz
+
+    # Kick Sauber
+    "HUL": "#00FF00",  # Hülkenberg - Verde neón de Kick Sauber
+    "BOR": "#00FF00",  # Bortoleto
+
+    # Haas
+    "OCO": "#B0B6B8",  # Ocon - Gris plateado de Haas
+    "BEA": "#B0B6B8",  # Bearman
+
+    # RB (Racing Bulls)
+    "LAW": "#6692FF",  # Lawson - Azul claro de RB
+    "HAD": "#6692FF",  # Hadjar
+}
+
+
